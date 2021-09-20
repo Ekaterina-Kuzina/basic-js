8d21ab03edac5795198be68d5f8420052e6f9b5e
Ekaterina Kuzina <katya.cuzina2011@yandex.ru>
2021-09-20 15:40:33 +0300

    feat:make task "Get email domain"

--- src/st-get-email-domain.js
+++ src/st-get-email-domain.js
@@ -10,7 +10,14 @@ import { NotImplementedError } from '../extensions/index.js';
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-export default function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getEmailDomain(email) {
+  let arr = email
+    .split('')
+  let ind = 0;
+  arr.forEach((element, index) => {
+    if (element == "@") {
+      ind = index
+    }
+  });
+  return arr.slice(ind + 1).join('')
 }
